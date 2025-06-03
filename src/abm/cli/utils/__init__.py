5404533e79eafaf284e8734826050f010cc70218
Marcell Szegedi <szegedi.marcell@gmail.com>
2025-06-03 20:17:08 +0200

    push to check workflows

--- src/abm/cli/utils/__init__.py
+++ src/abm/cli/utils/__init__.py
@@ -1,1 +1,14 @@
 """Entry point of the utils directory in CLI."""
+
+import numpy as np
+import numpy.typing as npt
+import scipy
+
+
+def main(alma: npt.NDArray[np.float64]) -> npt.NDArray[np.float64]:
+    """Example function to test mypy static type checking."""
+    return alma + 1
+
+
+if __name__ == "__main__":
+    main("alma")
