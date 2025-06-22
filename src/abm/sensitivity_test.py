88ffe31558df99ff5990b41ee0132f6fe7fb288d
znasser60 <znnasser01@gmail.com>
2025-06-22 21:05:54 +0200

    add morris sensitivity algorithm

--- src/abm/sensitivity_test.py
+++ src/abm/sensitivity_test.py
@@ -1,8 +1,8 @@
 """Module for performing sensitivity tests on the Riot model."""
 
 import numpy as np
-from SALib.analyze import sobol
-from SALib.sample import saltelli
+from SALib.analyze import sobol, morris 
+from SALib.sample import saltelli, morris as morris_sampling
 from typer import Callable
 
 from abm.utils import global_model_parameters as params
@@ -29,13 +29,14 @@ class SensitivityTests:
             np.ndarray: Array of final fractions of rioters after the simulation.
         """
         results = []
-        for i, x in enumerate(sample):
+        for x in sample:
             (
                 params.INITIAL_PROB_OF_BASE,
                 params.INITIAL_PROB_OF_RIOT,
                 params.INITIAL_ROUND_OF_ENTRY_HOME,
                 params.INITIAL_ROUND_OF_ENTRY_AWAY,
-            ) = x #TODO: Continue with remaining parameters regarding urban design
+            ) = x # Sets global parameter values based on sample drawn.
+            #TODO: Continue with remaining parameters regarding urban design.
 
             agent_state, _ = self.model_run(
                 width=100,
@@ -43,9 +44,10 @@ class SensitivityTests:
                 entry_point_home=(10, 0),
                 entry_point_away=(90, 0),
                 n_step=self.steps
-            ) #TODO: Add these to global params as to not hardcode? 
+            ) # Runs the model with the given parameters.
+            #TODO: Add these to global params as to not hardcode? 
 
-            final_rioters = agent_state["Rioter"].iloc[-1]
+            final_rioters = agent_state["Rioter"].iloc[-1] 
             total = agent_state.iloc[-1].sum()
             results.append(final_rioters / total if total else 0)
 
@@ -60,5 +62,27 @@ class SensitivityTests:
         test_values = saltelli.sample(self.problem, self.num_samples)
         sample_riot_fractions = np.array([self.model_run(params) for params in test_values])
         return sobol.analyze(self.problem, sample_riot_fractions)
+    
+    def morris_sensitivity_test(self) -> dict:
+        """Performs Morris sensitivity analysis on the model.
+        
+        Returns:
+            dict: Morris sensitivity indices (mu, mu_star, etc.)
+        """
+        test_values = morris_sampling.sample(self.problem, self.num_samples)
+        sample_riot_fractions = np.array([self.model_run(params) for params in test_values])
+        return morris.analyze(self.problem, sample_riot_fractions)
+    
+    def ofat_sensitivity_test(): 
+        """Performs One Factor At a Time (OFAT) sensitivity analysis on the model.
+        
+        Returns:
+            dict: Results of the OFAT sensitivity analysis.
+        """
+        # Placeholder for OFAT implementation
+        raise NotImplementedError("OFAT sensitivity test is not implemented yet.")
+    
+
+
     
     
